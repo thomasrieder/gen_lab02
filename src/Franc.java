34a051b83c7e1d8542b197efeda0e3c34324e2a2
thomas rieder <thom.rieder@gmail.com>
2020-03-24 13:43:36 +0100

    WIP #1, N.30

---
+++ src/Franc.java
@@ -0,0 +1,20 @@
+public class Franc {
+
+    private int amount;
+
+    public Franc(int amount){
+
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+
+        return new Franc(this.amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+
+        Franc franc = (Franc) object;
+        return this.amount == franc.amount;
+    }
+}
