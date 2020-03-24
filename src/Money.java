ad0ff8eb31ecc29e6918995adc29b9d4099c3a98
thomas rieder <thom.rieder@gmail.com>
2020-03-24 14:31:46 +0100

    WIP #13, N.37, N.38

--- src/Money.java
+++ src/Money.java
@@ -1,12 +1,18 @@
-public class Money {
+abstract public class Money {
 
     protected int amount;
+
+    abstract Money times(int multiplier);
 
 
     public boolean equals(Object object) {
 
         Money money = (Money) object;
         return this.amount == money.amount && getClass().equals(money.getClass());
+    }
+
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
     }
 }
 
