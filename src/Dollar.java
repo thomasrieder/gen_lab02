f237833bf3fa54bac12c1301f545ea5ee9e94675
thomas rieder <thom.rieder@gmail.com>
2020-03-24 13:10:57 +0100

    WIP #2, N.20

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount){
+
+    }
+
+    void times(int multiplier) {
+
+        this.amount = 5 * 2;
+    }
 }
--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MoneyTest {
+
+    public MoneyTest() {
+
+    }
+
+    @Test
+    public void testMultiplication() {
+
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
 }
